81394e12e1e6c69ee8b8d1f25c61c1e111f669e2
william Ruiz <william.a23@gmail.com>
2022-10-24 13:46:19 -0500

    primer commit madlibs

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+from cgi import print_form
+
+
+adj = input("Adjetive: ")
+verb1 = input("verbo1: ")
+verb2 = input("verbo2: ")
+famous_person = input("FP: ")
+
+madlib = f"Computer programming is so {adj}!! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print (madlib)
+
+
